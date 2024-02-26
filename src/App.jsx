0b2f44c6fa4593edd0b13f919ff0d713b5b5aa9b
Sailes123 <saileshbajgain056@gmail.com>
2024-02-26 16:23:28 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+
+
+function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm Bot! Ask me anything!",
+      sentTime: "just now",
+      sender: "Bot"
+    }
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: "user"
+    };
+
+    await processMessage(newMessage);
+  };
+
+  async function processMessage(chatMessages) { 
+
+    console.log('chatMessage', chatMessages) // TODO: You can watch what messages is there.
+
+   // TODO: change url
+
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(chatMessages?.message??'') // send data from frontend
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);   // TODO : you can see what data is coming from backend
+      setMessages([...chatMessages, {
+        message: data?.message??'', // TODO: 
+        sender: "Bot"
+      }]);
+      setIsTyping(false);
+    });
+  }
+
+  return (
+    <div className="App">
+      <div style={{ position:"relative", height: "800px", width: "700px"  }}>
+        <MainContainer>
+          <ChatContainer>       
+            <MessageList 
+              scrollBehavior="smooth" 
+              typingIndicator={isTyping ? <TypingIndicator content="Bot is typing" /> : null}
+            >
+              {messages.map((message, i) => {
+                console.log(message)
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />        
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+  )
+}
+
+export default App
